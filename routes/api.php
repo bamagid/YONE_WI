40f42de99e46df41a130d7934e22c32e72999d26
bamagid <bamagid60@gmail.com>
2024-01-21 04:46:33 +0000

    Là j'ai generer la documentation swagger et les routes pour tester les fonctionnalités

--- routes/api.php
+++ routes/api.php
@@ -1,39 +1,20 @@
 <?php
 
 use App\Http\Controllers\AdminSystemController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReseauController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\SearchController;
 
 Route::post("login", [UserController::class, "login"]);
-Route::post("loginadmin", [AdminSystemController::class, "login"]);
-
-Route::group([
-    "middleware" => ["auth:api"]
-], function () {
-
-    Route::get("profile", [UserController::class, "profile"]);
-    Route::get("refresh", [UserController::class, "refreshToken"]);
-    Route::get("logout", [UserController::class, "logout"]);
-});
-
-Route::group([
-    "middleware" => ["auth:admin"]
-], function () {
-    Route::resource('users', UserController::class);
-    Route::resource('roles', RoleController::class);
-    Route::resource('reseaux', ReseauController::class);
-});
+Route::get("profile", [UserController::class, "profile"]);
+Route::get("refresh", [UserController::class, "refreshToken"]);
+Route::get("logout", [UserController::class, "logout"]);
+Route::patch('updateadmin', [AdminSystemController::class, 'update']);
+Route::resource('users', UserController::class)->except('edit', 'create', 'show');
+Route::get('users/etat', [UserController::class, "changerEtat"]);
+Route::resource('roles', RoleController::class)->except(['show', 'create', 'edit']);
+Route::get("roles/restaure/{role}", [RoleController::class, "restore"]);
+Route::resource('reseaux', ReseauController::class);
+Route::post('search', [SearchController::class, 'searching']);
