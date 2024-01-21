7431b3356f4da83084c7ead8a7e92b57b84bba86
bamagid <bamagid60@gmail.com>
2024-01-21 04:26:18 +0000

    J'ai traduis toutes les execeptions de laravel sous format json dans ce fichier

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -18,13 +18,26 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+
+    public function render($request, Throwable $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json([
+                'error' => $exception->getMessage(),
+            ], $exception->getStatusCode() ?: 400);
+        }
+
+        return parent::render($request, $exception);
+    }
     /**
      * Register the exception handling callbacks for the application.
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Throwable $e) {
+            return response()->json([
+                'error' => $e->getMessage(),
+            ], $e->getStatusCode() ?: 400);
         });
     }
 }
